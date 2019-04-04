905e2174ebd17a23ab2e91cc6301b7545f107eb0
frede <frede@141.79.46.225>
2019-04-04 12:39:54 +0200

    Methode parse implementiert aber nicht getestet

--- src/main/java/org/ai2ra/hso/simpic16f84/sim/LstParser.java
+++ src/main/java/org/ai2ra/hso/simpic16f84/sim/LstParser.java
@@ -1,11 +1,53 @@
 package org.ai2ra.hso.simpic16f84.sim;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class LstParser {
 		
-	public int[] parse(File file) {
+	public static int[] parse(File file) throws IOException {
 		
-		return null;
+		FileReader input = new FileReader(file);
+		BufferedReader inputfile = new BufferedReader(input);
+		ArrayList<String> converter = new ArrayList<String>(); 
+		String testLine = "";
+		
+		/* loop that checks every line of a given LST file for machine language 
+		 * and saves it into an ArrayList as a String. 
+		*/
+		while (testLine != null) {
+			
+			testLine = inputfile.readLine();
+			
+			if( testLine.startsWith(" ")) {
+							
+			}else {
+				
+			converter.add(testLine.substring(5, 8));	
+				
+			} 
+		}
+		inputfile.close();
+		
+		/* Array that will output the machine code 
+		 */
+		int[] output = new int[converter.size()];
+		
+		/* Loop that decodes the Strings of the Arraylist into an int and saves it into the output Array.
+		 * The input string is decoded from Hex to int.
+		 * The index of the output Array corresponds to the index of the machine code.
+		*/
+		for (int i = 0; i < converter.size(); i++) {
+			int insert = Integer.decode("0x" + converter.get(i));
+			output[i] = insert;
+		}
+		
+		return output;
 	}
+	
+	
+	
 }
