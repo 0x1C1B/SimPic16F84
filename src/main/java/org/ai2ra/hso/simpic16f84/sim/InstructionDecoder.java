cc8a858863c0514d4019f777491c6e1cb7b989be
0x1C1B <32311121+0x1C1B@users.noreply.github.com>
2019-04-04 11:38:30 +0200

    Initialize instruction decoder with related unit test

---
+++ src/main/java/org/ai2ra/hso/simpic16f84/sim/InstructionDecoder.java
@@ -0,0 +1,9 @@
+package org.ai2ra.hso.simpic16f84.sim;
+
+public class InstructionDecoder {
+
+    public static int[] decode(int instruction) {
+
+        return null;
+    }
+}
---
+++ src/test/java/org/ai2ra/hso/simpic16f84/sim/InstructionDecoderTest.java
@@ -0,0 +1,7 @@
+package org.ai2ra.hso.simpic16f84.sim;
+
+import static org.junit.Assert.*;
+
+public class InstructionDecoderTest {
+
+}
