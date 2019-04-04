d815e22c2f669659d9b9d03c12c2d91cb229f806
frede <frede@141.79.46.225>
2019-04-04 11:08:44 +0200

    Pakete angelegt: sim und testsim Klassen angelegt: LstParser und LstParserTest

---
+++ src/main/java/org/ai2ra/hso/simpic16f84/sim/LstParser.java
@@ -0,0 +1,11 @@
+package org.ai2ra.hso.simpic16f84.sim;
+
+import java.io.File;
+
+public class LstParser {
+		
+	public int[] parse(File file) {
+		
+		return null;
+	}
+}
---
+++ src/test/java/org/ai2ra/hso/simpic16f84/sim/LstParserTest.java
@@ -0,0 +1,14 @@
+package org.ai2ra.hso.simpic16f84.sim;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LstParserTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
