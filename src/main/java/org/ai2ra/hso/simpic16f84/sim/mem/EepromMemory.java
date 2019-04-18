3dd97008aab5869d53c918af7c07b34659d87a6c
Freddy1096 <frederic.karbowiak@web.de>
2019-04-18 11:28:07 +0200

    EEPROM Synchronisiert

--- src/main/java/org/ai2ra/hso/simpic16f84/sim/mem/EepromMemory.java
+++ src/main/java/org/ai2ra/hso/simpic16f84/sim/mem/EepromMemory.java
@@ -3,77 +3,141 @@ package org.ai2ra.hso.simpic16f84.sim.mem;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.util.Arrays;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class EepromMemory<T> implements ObservableMemory<T> {
 
 
     private T[] memory;
     private PropertyChangeSupport changes;
+    private ReadWriteLock lock;
 
-    @SuppressWarnings("unchecked")
+
     public EepromMemory(int size) {
 
         this.memory = (T[]) new Object[size];
         changes = new PropertyChangeSupport(this);
+        lock = new ReentrantReadWriteLock();
 
     }
 
     @Override
     public void addPropertyChangeListener(PropertyChangeListener listener) {
-        changes.addPropertyChangeListener(listener);
+
+        lock.writeLock().lock();
+
+        try {
+
+            changes.addPropertyChangeListener(listener);
+
+        }finally {
+
+            lock.writeLock().unlock();
+        }
+
     }
 
     @Override
     public void removePropertyChangeListener(PropertyChangeListener listener) {
-        changes.removePropertyChangeListener(listener);
+
+        lock.writeLock().lock();
+
+        try {
+
+            changes.removePropertyChangeListener(listener);
+
+        }finally {
+
+            lock.writeLock().unlock();
+
+        }
+
+
     }
 
     @Override
     public T get(int address) throws MemoryIndexOutOfBoundsException {
 
-        if (memory.length == 0 || address > memory.length || address < 0){
+        lock.readLock().lock();
 
-            throw new MemoryIndexOutOfBoundsException();
+        try {
+
+            if (memory.length == 0 || address > memory.length || address < 0) {
+
+                throw new MemoryIndexOutOfBoundsException();
+
+            }
+
+            return memory[address];
+
+        }finally {
+
+            lock.readLock().unlock();
 
         }
-        return memory[address];
-
     }
 
     @Override
     public T[] fetch() {
 
-        if (memory.length == 0){
+        lock.readLock().lock();
 
-            throw new MemoryIndexOutOfBoundsException();
+        try {
+
+            if (memory.length == 0) {
+
+                throw new MemoryIndexOutOfBoundsException();
+
+            }
+            return Arrays.copyOf(memory, memory.length);
+
+        }finally {
+
+            lock.readLock().unlock();
 
         }
-        return Arrays.copyOf(memory, memory.length);
     }
 
 
     public void set(T toSet, int address) {
 
-        if (memory.length == 0 || address > memory.length || address < 0){
+        lock.writeLock().lock();
 
-            throw new MemoryIndexOutOfBoundsException();
+        try {
 
-        }else{
-            T beforeSet = memory[address];
-            this.memory[address] = toSet;
-            changes.firePropertyChange(String.format("memory[%d]", address), beforeSet, toSet);
+            if (memory.length == 0 || address > memory.length || address < 0) {
+
+                throw new MemoryIndexOutOfBoundsException();
+
+            } else {
+                T beforeSet = memory[address];
+                this.memory[address] = toSet;
+                changes.firePropertyChange(String.format("memory[%d]", address), beforeSet, toSet);
+
+            }
+        }finally {
+
+            lock.writeLock().unlock();
 
         }
-
     }
 
 
     public void reset(){
 
-        T[] copy = fetch();
-        Arrays.fill(memory, null);
-        changes.firePropertyChange("memory", copy, memory );
+        lock.writeLock().lock();
 
+        try {
+
+            T[] copy = fetch();
+            Arrays.fill(memory, null);
+            changes.firePropertyChange("memory", copy, memory);
+        }finally {
+
+            lock.writeLock().unlock();
+
+        }
     }
 }
 
