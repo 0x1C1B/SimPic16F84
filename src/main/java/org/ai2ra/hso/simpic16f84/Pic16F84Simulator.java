5fed819c01ae8e9f2e0baf3c24ef458db0193a43
0x1C1B <32311121+0x1C1B@users.noreply.github.com>
2019-04-23 10:44:37 +0200

    Initialize JavaFX based application entry

--- src/main/java/org/ai2ra/hso/simpic16f84/Pic16F84Simulator.java
+++ src/main/java/org/ai2ra/hso/simpic16f84/Pic16F84Simulator.java
@@ -1,8 +1,18 @@
 package org.ai2ra.hso.simpic16f84;
 
-public class Pic16F84Simulator {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Pic16F84Simulator extends Application {
+
+    @Override public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("Pic16F84Simulator");
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
 
+        launch(args);
     }
 }
