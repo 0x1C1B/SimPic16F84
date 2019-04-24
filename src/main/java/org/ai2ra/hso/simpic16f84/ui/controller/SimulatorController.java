3491e4a329c3ad675446106687d0474cb3dc2a18
0x1C1B <32311121+0x1C1B@users.noreply.github.com>
2019-04-24 08:09:31 +0200

    Provide file chooser for loading new LST file

--- src/main/java/org/ai2ra/hso/simpic16f84/ui/controller/SimulatorController.java
+++ src/main/java/org/ai2ra/hso/simpic16f84/ui/controller/SimulatorController.java
@@ -1,12 +1,17 @@
 package org.ai2ra.hso.simpic16f84.ui.controller;
 
 import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.stage.FileChooser;
 import org.fxmisc.richtext.CodeArea;
 import org.fxmisc.richtext.LineNumberFactory;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -22,5 +27,53 @@ public class SimulatorController implements Initializable {
     @FXML private void onQuitAction(ActionEvent event) {
 
         Platform.exit();
+    }
+
+    @FXML private void onOpenAction(ActionEvent event) {
+
+        FileChooser fileChooser = new FileChooser();
+
+        fileChooser.setTitle("Open LST File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("LST Files", "*.LST")
+        );
+
+        File file = fileChooser.showOpenDialog(null);
+
+        if(null != file) {
+
+            Task<String> task = new Task<String>() {
+
+                @Override protected String call() throws Exception {
+
+                    StringBuilder builder = new StringBuilder();
+
+                    try(BufferedReader reader = new BufferedReader(new FileReader(file))) {
+
+                        String line;
+
+                        while(null != (line = reader.readLine())) {
+
+                            builder.append(line).append(System.lineSeparator());
+                        }
+                    }
+
+                    return builder.toString();
+                }
+            };
+
+            task.setOnSucceeded((evt) -> {
+
+                lstView.replaceText(task.getValue());
+                lstView.moveTo(0, 0);
+            });
+
+            task.setOnFailed((evt) -> {
+
+                task.getException().printStackTrace(System.err);
+            });
+
+            new Thread(task).start();
+        }
     }
 }
