588ef23b299177ed449cadf4e00d25d12738c3bd
0x1C1B <32311121+0x1C1B@users.noreply.github.com>
2019-04-24 07:42:19 +0200

    Add rich text area for displaying content of the LST file

--- src/main/java/org/ai2ra/hso/simpic16f84/ui/controller/SimulatorController.java
+++ src/main/java/org/ai2ra/hso/simpic16f84/ui/controller/SimulatorController.java
@@ -3,8 +3,21 @@ package org.ai2ra.hso.simpic16f84.ui.controller;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.LineNumberFactory;
 
-public class SimulatorController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SimulatorController implements Initializable {
+
+    @FXML private CodeArea lstView;
+
+    @Override public void initialize(URL location, ResourceBundle resources) {
+
+        lstView.setParagraphGraphicFactory(LineNumberFactory.get(lstView));
+    }
 
     @FXML private void onQuitAction(ActionEvent event) {
 
