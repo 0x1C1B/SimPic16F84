88aa96ca311bccfa3615674e762a7e1d3e8118ba
0x1C1B <32311121+0x1C1B@users.noreply.github.com>
2019-04-24 13:35:20 +0200

    Insert breakpoints as graphical feature for additional control about the later runtime flow

--- src/main/java/org/ai2ra/hso/simpic16f84/ui/controller/SimulatorController.java
+++ src/main/java/org/ai2ra/hso/simpic16f84/ui/controller/SimulatorController.java
@@ -1,14 +1,27 @@
 package org.ai2ra.hso.simpic16f84.ui.controller;
 
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableSet;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.HBox;
 import javafx.stage.FileChooser;
+import org.ai2ra.hso.simpic16f84.ui.util.BreakpointFactory;
 import org.ai2ra.hso.simpic16f84.ui.util.SyntaxHighlighting;
 import org.fxmisc.richtext.CodeArea;
 import org.fxmisc.richtext.LineNumberFactory;
+import org.fxmisc.richtext.model.TwoDimensional;
+import org.fxmisc.wellbehaved.event.InputMap;
+import org.fxmisc.wellbehaved.event.Nodes;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -16,20 +29,49 @@ import java.io.FileReader;
 import java.net.URL;
 import java.time.Duration;
 import java.util.ResourceBundle;
+import java.util.function.IntFunction;
+
+import static org.fxmisc.wellbehaved.event.EventPattern.*;
 
 public class SimulatorController implements Initializable {
 
     @FXML private CodeArea lstView;
+    private ObservableSet<Integer> breakpoints;
+
+    public SimulatorController() {
+
+        breakpoints = FXCollections.observableSet();
+    }
 
     @Override public void initialize(URL location, ResourceBundle resources) {
 
-        lstView.setParagraphGraphicFactory(LineNumberFactory.get(lstView));
+        IntFunction<Node> numberFactory = LineNumberFactory.get(lstView);
+        IntFunction<Node> breakpointFactory = new BreakpointFactory(breakpoints);
+
+        IntFunction<Node> graphicFactory = line -> {
+
+            HBox hbox = new HBox(numberFactory.apply(line),
+                    breakpointFactory.apply(line));
+
+            hbox.setAlignment(Pos.CENTER_LEFT);
+            return hbox;
+        };
+
+        lstView.setParagraphGraphicFactory(graphicFactory);
 
         // Enable syntax highlighting, updated all 500 ms after typing is stopped/ text is set
 
         lstView.multiPlainChanges()
                 .successionEnds(Duration.ofMillis(500))
                 .subscribe(change -> lstView.setStyleSpans(0, SyntaxHighlighting.compute(lstView.getText())));
+
+        // Enable key listeners for setting breakpoints
+
+        InputMap<Event> inputMap = InputMap.consume(
+                keyPressed(new KeyCodeCombination(KeyCode.B, KeyCombination.CONTROL_DOWN)),
+                (ignore) -> toggleBreakpoint());
+
+        Nodes.addInputMap(lstView, inputMap);
     }
 
     @FXML private void onQuitAction(ActionEvent event) {
@@ -82,6 +124,35 @@ public class SimulatorController implements Initializable {
             });
 
             new Thread(task).start();
+        }
+    }
+
+    @FXML private void onBreakpointAction(ActionEvent event) {
+
+        toggleBreakpoint();
+    }
+
+    /**
+     * Toggles a breakpoint for the currently selected line inside of
+     * the LST view. With selection, the current caret position (line)
+     * is meant. If breakpoint is already set, it is removed otherwise
+     * it is set.
+     *
+     * @see #lstView
+     */
+
+    private void toggleBreakpoint() {
+
+        int line = lstView.offsetToPosition(
+                lstView.getCaretPosition(), TwoDimensional.Bias.Forward).getMajor();
+
+        if(breakpoints.contains(line)) {
+
+            breakpoints.remove(line);
+
+        } else {
+
+            breakpoints.add(line);
         }
     }
 }
---
+++ src/main/java/org/ai2ra/hso/simpic16f84/ui/util/BreakpointFactory.java
@@ -0,0 +1,48 @@
+package org.ai2ra.hso.simpic16f84.ui.util;
+
+import javafx.collections.ObservableSet;
+import javafx.collections.SetChangeListener;
+import javafx.scene.Node;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+import java.util.function.IntFunction;
+
+/**
+ * This class is responsible for rendering the breakpoints inside of a
+ * {@link org.fxmisc.richtext.CodeArea CodeArea}.
+ *
+ * @author 0x1C1B
+ */
+
+public class BreakpointFactory implements IntFunction<Node> {
+
+    private ObservableSet<Integer> breakpoints;
+
+    public BreakpointFactory(ObservableSet<Integer> breakpoints) {
+
+        this.breakpoints = breakpoints;
+    }
+
+    @Override public Node apply(int line) {
+
+        Circle breakpoint = new Circle(3);
+        breakpoint.setFill(Color.RED);
+        breakpoint.setVisible(false);
+
+        // Allow dynamic removing/adding of the breakpoint
+
+        breakpoints.addListener((SetChangeListener<Integer>) change -> {
+
+            int changed = change.wasAdded() ? change.getElementAdded() :
+                    change.getElementRemoved();
+
+            if(changed == line) {
+
+                breakpoint.setVisible(change.wasAdded());
+            }
+        });
+
+        return breakpoint;
+    }
+}
