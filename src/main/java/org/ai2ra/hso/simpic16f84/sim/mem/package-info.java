f2a187cb9f41bbe1d0a5457ae8a5e0166da7417c
0x1C1B <32311121+0x1C1B@users.noreply.github.com>
2019-05-23 20:41:39 +0200

    Update major package documentation

---
+++ src/main/java/org/ai2ra/hso/simpic16f84/sim/mem/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Required memory blocks of the Pic16F84 simulator are implemented inside
+ * this package. Each memory block is separated in a single observable class.
+ */
+
+package org.ai2ra.hso.simpic16f84.sim.mem;
